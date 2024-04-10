0c57e4e6748f89ef7e4158122897aa82849acedd
biancasio <161092766+biancasio@users.noreply.github.com>
2024-04-10 16:37:55 +0000

    feat: criacao da variavel max_descricao e max_categoria

--- tarefas.c
+++ tarefas.c
@@ -10,10 +10,10 @@ ERROS criar(Tarefa tarefas[], int *pos){
     scanf("%d", &tarefas[*pos].prioridade);
     clearBuffer();
     printf("Entre com a categoria: ");
-    fgets(tarefas[*pos].categoria, 100, stdin);
+    fgets(tarefas[*pos].categoria, MAX_CATEGORIA, stdin);
 
     printf("Entre com a descricao: ");
-    fgets(tarefas[*pos].descricao, 300, stdin);
+    fgets(tarefas[*pos].descricao, MAX_DESCRICAO, stdin);
 
     *pos = *pos + 1;
 

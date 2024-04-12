ff3bdc5445bcd8b8c0f359a2e0431b946abc8420
Linejesus <161092862+Linejesus@users.noreply.github.com>
2024-04-12 19:36:55 +0000

    Erros tratados na função main

--- main.c
+++ main.c
@@ -7,8 +7,18 @@ int main(){
     Tarefa tarefas[TOTAL];
     int pos;
     ERROS erro = fs[4](tarefas, &pos);
-    if(erro != OK)
+    if (erro == ABRIR) {
+        printf("Erro ao carregar o arquivo para abrir.\n");
         pos = 0;
+
+    } else if (erro == FECHAR) {
+        printf("Erro ao carregar o arquivo para fechar.\n");
+        pos = 0;
+
+    } else if (erro == LER) {
+        printf("Erro ao carregar o arquivo para ler.\n");
+        pos = 0;
+    }
 
     int opcao;
     do{
@@ -21,14 +31,37 @@ int main(){
 
         scanf("%d", &opcao);
         opcao--;
-        if(opcao > 2)
+        if(opcao > 2){
             printf("Opcao invalida\n");
-        else if(opcao >= 0)
-            fs[opcao](tarefas, &pos);
+        } else if (opcao == 0) {
+            erro = fs[opcao](tarefas, &pos);
+            if (erro == MAX_TAREFA) {
+                printf("Máximo de tarefas alcançadas\n");
+            }
+        } else if (opcao == 1) {
+            erro = fs[opcao](tarefas, &pos);
+            if (erro == SEM_TAREFAS) {
+                printf("Sem tarefas para deletar\n");
+            } else if (erro == NAO_ENCONTRADO) {
+                printf("Tarefa não existe\n");
+            }
+        } else if (opcao == 2) {
+            erro = fs[opcao](tarefas, &pos);
+            if (erro == NAO_ENCONTRADO) {
+                printf("Sem tarefas para listar\n");
+            }
+        }
         else
             printf("Sair...\n");
 
     } while(opcao >= 0);
 
-    fs[3](tarefas, &pos);
+    erro = fs[3](tarefas, &pos);
+    if (erro == ABRIR) {
+        printf("Erro para abrir o arquivo ao salvar\n");
+    } else if (erro == FECHAR) {
+        printf("Erro para fechar o arquivo ao salvar\n");
+    } else if (erro == ESCREVER) {
+        printf("Erro ao escrever no arquivo ao salvar\n");
+    }
 }

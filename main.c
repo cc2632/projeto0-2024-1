dffdd93af833bb51ea614325a160d646e814c54a
Mario Eugenio Silva <161092737+Mario9809@users.noreply.github.com>
2024-04-12 22:59:29 +0000

    correcao de bug na funcao main, falta corrigir

--- main.c
+++ main.c
@@ -24,13 +24,51 @@ int main() {
     opcao--;
     if (opcao  > 5)
       printf("Opcao invalida\n");
-    else if (opcao >= 0)
-      fs[opcao](tarefas, &pos);
-    else if (opcao == 3)
-      fs[opcao](tarefas, &pos);
-    else
+    else if (opcao >= 0) {
+      ERROS resultado = fs[opcao](tarefas, &pos);
+      if (resultado != OK) {
+        switch (resultado) {
+          case MAX_TAREFA:
+            printf("Erro: Numero maximo de tarefas atingido!\n");
+            break;
+          case SEM_TAREFAS:
+            printf("Erro: Nao ha tarefas para realizar essa operacao!\n");
+            break;
+          case NAO_ENCONTRADO:
+            printf("Erro: Tarefa nao encontrada!\n");
+            break;
+          case ABRIR:
+            printf("Erro: Nao foi possivel abrir o arquivo!\n");
+            break;
+          case FECHAR:
+            printf("Erro: Nao foi possivel fechar o arquivo!\n");
+            break;
+          case ESCREVER:
+            printf("Erro: Nao foi possivel escrever no arquivo!\n");
+            break;
+          case LER:
+            printf("Erro: Nao foi possivel ler do arquivo!\n");
+            break;
+          default:
+            printf("Erro desconhecido!\n");
+            break;
+        }
+      }
+    } else if (opcao == 3) {
+      ERROS resultado = fs[opcao](tarefas, &pos);
+      if (resultado != OK) {
+        switch (resultado) {
+          case SEM_TAREFAS:
+            printf("Erro: Nao ha tarefas para listar!\n");
+            break;
+          default:
+            printf("Erro desconhecido!\n");
+            break;
+        }
+      }
+    } else {
       printf("Sair...\n");
-
+    }
   } while (opcao >= 0);
 
   fs[3](tarefas, &pos);

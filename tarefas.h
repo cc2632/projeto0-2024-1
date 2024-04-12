b9ed599df9f4a8113ffd3cf503521df2dc47e203
GustavoLimadeAzevedo <122912072+GustavoLimadeAzevedo@users.noreply.github.com>
2024-04-12 16:11:55 -0300

    Atualizado tarefas.h

--- tarefas.h
+++ tarefas.h
@@ -1,3 +1,4 @@
+// tarefas.h
 #define TOTAL 100
 
 typedef struct {
@@ -6,13 +7,13 @@ typedef struct {
     char categoria[100];
 } Tarefa;
 
-typedef enum {OK, MAX_TAREFA, SEM_TAREFAS, NAO_ENCONTRADO, ABRIR, FECHAR, ESCREVER, LER} ERROS;
+typedef enum ERROS {OK, MAX_TAREFA, SEM_TAREFAS, NAO_ENCONTRADO, ABRIR, FECHAR, ESCREVER, LER} ERROS;
 
-typedef ERROS (*funcao)(Tarefa[], int*);
+typedef ERROS (funcao)(Tarefa[], int*);
 
 ERROS criar(Tarefa tarefas[], int *pos);
 ERROS deletar(Tarefa tarefas[], int *pos);
-ERROS listar(Tarefa tarefas[], int *pos);
+ERROS listar(Tarefa tarefas[], int pos);
 ERROS salvar(Tarefa tarefas[], int *pos);
 ERROS carregar(Tarefa tarefas[], int *pos);
 

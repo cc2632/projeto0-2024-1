11b47fe52124bd2f45298243e437bd392711ace6
Luana Almeida <134015897+Luaalmed@users.noreply.github.com>
2024-04-12 13:01:55 +0000

    implementação das opções salvar e carregar no menu principal

--- main.c
+++ main.c
@@ -6,7 +6,9 @@ int main(){
 
     Tarefa tarefas[TOTAL];
     int pos;
-    ERROS erro = fs[4](tarefas, &pos);
+    
+    // Carrega as tarefas do arquivo binário
+    ERROS erro = fs[4](tarefas, &pos); 
     if(erro != OK)
         pos = 0;
 
@@ -16,19 +18,26 @@ int main(){
         printf("1 - Criar tarefa\n");
         printf("2 - Deletar tarefa\n");
         printf("3 - Listar tarefas\n");
+        printf("4 - Salvar tarefas\n"); // Adiciona a opção de salvar tarefas
+        printf("5 - Carregar tarefas\n"); // Adiciona a opção de carregar tarefas
         printf("0 - Sair\n");
         printf("Escolha uma opcao: ");
 
         scanf("%d", &opcao);
-        opcao--;
-        if(opcao > 2)
+        if(opcao > 5 || opcao < 0) {
             printf("Opcao invalida\n");
-        else if(opcao >= 0)
-            fs[opcao](tarefas, &pos);
-        else
+            continue; // Volta para o início do loop
+        }
+
+        if(opcao == 0)
             printf("Sair...\n");
+        else if(opcao == 4 || opcao == 5)
+            fs[opcao-1](tarefas, &pos); // Chama a função correspondente à opção
+        else
+            fs[opcao-1](tarefas, &pos);
 
-    } while(opcao >= 0);
+    } while(opcao != 0);
 
-    fs[3](tarefas, &pos);
+    return 0;
 }
+ 
